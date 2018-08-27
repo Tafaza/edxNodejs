4ff019f2a89a128322f8682a6997082cf4e652e7
isakhito <isacco.cattabiani@protonmail.com>
2018-08-27 15:30:44 +0200

    Add readme and update the code to works with zero o one parameters

--- Lab1/csv_to_json_converter/Csv2JsonConverter.js
+++ Lab1/csv_to_json_converter/Csv2JsonConverter.js
@@ -3,17 +3,23 @@ const request=require('request')
 const fs = require('fs');
 const csv=require('csvtojson')
 
-const csvUrl = "https://prod-edxapp.edx-cdn.org/assets/courseware/v1/07d100219da1a726dad5eddb090fa215/asset-v1:Microsoft+DEV283x+2T2018+type@asset+block/customer-data.csv"
-
-csv()
-.fromStream(request.get(csvUrl))
-.then((obj) => {
-    console.log("Content downloaded and converted")
-    var json = JSON.stringify(obj, null, 2)
-    fs.writeFileSync("customare-data.json", json, 'utf8')
-    console.log("Json written on a file")
-})
+var csvUrlLab1 = process.argv[2]
+if (!csvUrlLab1)
+    csvUrlLab1 = "https://prod-edxapp.edx-cdn.org/assets/courseware/v1/07d100219da1a726dad5eddb090fa215/asset-v1:Microsoft+DEV283x+2T2018+type@asset+block/customer-data.csv"
 
 
+csvToJson = function(csvUrl = csvUrlLab1)
+{
+    csv()
+    .fromStream(request.get(csvUrl))
+    .then((obj) => {
+        console.log("Content downloaded and converted")
+        var json = JSON.stringify(obj, null, 2)
+        fs.writeFileSync("customare-data.json", json, 'utf8')
+        console.log("Json written on a file")
+    })
+}
+
+csvToJson(csvUrlLab1)
 
 
---
+++ Lab1/csv_to_json_converter/readme.md
@@ -0,0 +1,10 @@
+# Node.js edX lab1: csv2json
+
+Csv2json is an utility to convert an input csv url to a local json file. 
+This is a lab from edx Node.js course.
+
+## Instructions
+Executing it with no parameter: `node .\Csv2JsonConverter.js` it will convert the url of the lab;  
+otherwise: 
+`node .\Csv2JsonConverter.js http:\\localhost:3000\mycsv.csv`
+will convert the csv in the first parameter. 
