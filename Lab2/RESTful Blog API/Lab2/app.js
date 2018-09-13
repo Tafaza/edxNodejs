2f0f1a41236666d14666ae7df17260550c4a58ce
isakhito <isacco.cattabiani@protonmail.com>
2018-09-13 12:16:02 +0200

    Removed empty lines

--- Lab2/RESTful Blog API/Lab2/app.js
+++ Lab2/RESTful Blog API/Lab2/app.js
@@ -59,8 +59,6 @@ app.use('/posts', postsRouter);
 app.use('/posts/:id/comments', commentsRouter);
 
 
-
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
